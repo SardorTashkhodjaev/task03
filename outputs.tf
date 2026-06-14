cb6ad3247c7accab8e20e2ae840feabf60cef7a0
Sardor <tashkhodjaev.sardor@gmail.com>
2026-06-14 22:54:53 +0500

    next version

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
-rg_id
-sa_blob_endpoint
-vnet_id
+output "rg_id" {
+  value = azurerm_resource_group.rg_name.id
+}
+
+output "sa_blob_endpoint" {
+  value = azurerm_storage_account.storage_name.primary_blob_endpoint
+}
+
+output "vnet_id" {
+  value = azurerm_virtual_network.vnet_name.id
+}
--- variables.tf
+++ variables.tf
@@ -1,5 +1,35 @@
-variable "rg_name" {}
-variable "vnet_name" {}
-variable "subnet1_name" {}
-variable "subnet2_name" {}
-variable "storage_name" {}
+variable "rg_name" {
+  type        = string
+  description = "Resource Group name"
+}
+
+variable "vnet_name" {
+  type        = string
+  description = "Virtual network name"
+}
+
+variable "subnet1_name" {
+  type        = string
+  description = "Subnet 1 name"
+}
+
+variable "subnet2_name" {
+  type        = string
+  description = "Subnet 2 name"
+}
+
+variable "storage_name" {
+  type        = string
+  description = "Storage name"
+}
+
+variable "location" {
+  type        = string
+  description = "Where is located"
+}
+
+variable "tag" {
+  type        = map(string)
+  description = "My tag "
+
+}
